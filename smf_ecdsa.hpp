6a6d0301264de7135750635b979af79c2901a6ba
chen-yumin <chenyuminhello@163.com>
2025-05-06 15:41:06 +0800

    Update smf_ecdsa.hpp

--- smf_ecdsa.hpp
+++ smf_ecdsa.hpp
@@ -21,15 +21,7 @@
 
 #include <fstream>
 #include <cybozu/stream.hpp>
-//using std::begin; using std::end;
-//using namespace mcl::bn;
-//bn　中已有对Fp, Fr 定义，故不能再用模板
 
-//typedef mcl::FpT<> Fp;
-//struct tagZn;
-//typedef mcl::FpT<tagZn> Zn;
-//typedef mcl::bn::Fr Zn;
-//typedef mcl::EcT<Fp, Zn> Ec;
 
 typedef mcl::ecdsa::Fp Fp;
 typedef mcl::ecdsa::Zn Zn;
@@ -38,18 +30,12 @@ typedef mcl::bn::Fr Fr;
 typedef mcl::bn::G1 G1;
 typedef mcl::bn::G2 G2;
 
-//typedef mcl::bn::G1 G1;
-//typedef mcl::bn::G2 G2;
-//typedef mcl::bn::Fp12 Fp12;
-
-
-
-//typedef Fr Zn;
-//typedef mcl::EcT<Fp, Zn> Ec;
 
 
 static const uint8_t* STRE;
-static bool flag = false;
+static bool flag1 = false;
+static bool flag2 = false;
+static bool flag3 = false;
 static size_t FSIZE = 1000;
 static size_t MSIZE = 4;
 static size_t BSIZE = 32;
@@ -78,7 +64,7 @@ struct Sig {
     uint8_t* seed;
 };
 
-// 注意这里允许的字节数
+// note the allowd byte count
 struct Srm {
 	uint8_t m[10000];
 	uint8_t kf[640];
@@ -137,23 +123,7 @@ void Setup(PP& pp) {
     
 }
 
-/*
-SecretKey KG(PP& pp, mcl::bn::G1& P, mcl::bn::G2& Q) {
-	// should I use two keys?
-	Zn x1;
-	mcl::bn::Fr x2;
-	
-	
-	x1.setRand();
-	x2.setRand();
-	Ec pub;
-	pp.wm_g.mul(pub, x1);
-	pp.wm_h.init(pub, 256, 10); // 作为公开参数，g是可以的，但pub公钥是个人拥有的，放在这里不太合适。
-	mcl::bn::G1::mul(P, pp.P, x2);
-	mcl::bn::G2::mul(Q, pp.Q, x2);
-	return SecretKey {x1, x2};
-}
-*/
+
 
 KeyPair KG(PP& pp) {
 	SecretKey sk;
@@ -283,41 +253,17 @@ bool Tv(const Srm& srm, const uint8_t* mac) {
 }
 
 
-/*
-void frank(uint8_t* key, SecretKey& sec, PP& pp, Sig& sig, const uint8_t* msg, 
-     uint8_t* mac, uint8_t* hm, const mcl::bn::G1& P, const mcl::bn::G2& Q) {
-	
-	RAND_priv_bytes(sig.seed, 32);
-	uint8_t* k = key;
-	Tg(k, sig.seed, msg, mac);
-
-	if(!flag) {
-	    mcl::bn::Fp12 e1;
-	    //mcl::bn::initPairing(mcl::BN254);
-	    mcl::bn::pairing(e1, P,Q);
-	    mcl::bn::Fp12::pow(e1, e1, sec.x2);
-	    e1.serialize(hm, 600);
-	    flag = true;
-	}
-	
-	for(int i =0; i < 32*MSIZE; i++) {
-		hm[i+576] = mac[i];
-	}
-	Sg(pp, sig, hm, sec.x1);
-}
-*/
-
 void Frank(PP& pp, Aux& aux, Sig& sig, SecretKey& sec, PublicKey& pkr, 
            PublicKey& pkj, uint8_t* msg) {
 	RAND_priv_bytes(sig.seed, 32);
 	Tg(aux.key, sig.seed, msg, aux.mac);
-	if(!flag) {
+	if(!flag1) {
 	    mcl::bn::Fp12 e1;
 	    //mcl::bn::initPairing(mcl::BN254);
 	    mcl::bn::pairing(e1, pkr.P,pkj.Q);
 	    mcl::bn::Fp12::pow(e1, e1, sec.x2);
 	    e1.serialize(aux.hm, 600);
-	    flag = true;
+	    flag1 = true;
 	}
 	
 	for(int i =0; i < 32*MSIZE; i++) {
@@ -326,36 +272,17 @@ void Frank(PP& pp, Aux& aux, Sig& sig, SecretKey& sec, PublicKey& pkr,
 	Sg(pp, sig, aux.hm, sec.x1);
 }
 
-/*
-bool verify(uint8_t* key, SecretKey& sec, PP& pp, const Sig& sig, const uint8_t* msg,
-       uint8_t* mac, uint8_t* hm, const mcl::bn::G1& P, const mcl::bn::G2& Q) {
-		
-		uint8_t* k = key;
-		Tg(k, sig.seed, msg, mac);
-		if(!flag) {
-			mcl::bn::Fp12 e1;
-			mcl::bn::pairing(e1, P,Q);
-			mcl::bn::Fp12::pow(e1, e1, sec.x2);
-			e1.serialize(hm, 600);
-			flag = true;
-		}
-		
-		for(int i = 0; i < 32*MSIZE; i++) {
-			hm[i+576] = mac[i];
-	   }
-		return Sv(pp, sig, hm);
-}
-*/
+
 
 bool Verify(PP& pp, Aux& aux, Sig& sig, SecretKey& sec, PublicKey& pks, 
             PublicKey& pkj, uint8_t* msg) {
 	    Tg(aux.key, sig.seed, msg, aux.mac);
-		if(!flag) {
+		if(!flag2) {
 			mcl::bn::Fp12 e1;
 			mcl::bn::pairing(e1, pks.P,pkj.Q);
 			mcl::bn::Fp12::pow(e1, e1, sec.x2);
 			e1.serialize(aux.hm, 600);
-			flag = true;
+			flag2 = true;
 		}
 		
 		for(int i = 0; i < 32*MSIZE; i++) {
@@ -364,36 +291,7 @@ bool Verify(PP& pp, Aux& aux, Sig& sig, SecretKey& sec, PublicKey& pks,
 		return Sv(pp, pks, sig, aux.hm);		
 }
 
-/*
-void report(uint8_t* key, const uint8_t* msg, uint8_t* mac, Srm& srm,  const Sig& sig, Sigma& sigma) {
-	
-	uint8_t* k = key;
-	Tg(k, sig.seed, msg, mac);
-	int h = 0;
-	for(int i: BSELECT) {
-		
-		if(i == -1) break;
-		//int j = 0;
-		srm.index[h] = i;
-		for(int j = 0; j < 32; j++) {
-			//k = i*32;
-			srm.kf[h*32+j] = key[i*32+j];
-			//srm.m[h*BSIZE+j] = msg[i*BSIZE+j];
-		}
-		for(int j = 0; j < BSIZE; j++) {
-			srm.m[h*BSIZE+j] = msg[i*BSIZE+j];
-		}
-		h++;
-	}
-	//srm.m[h*32] = '\0';
-	srm.index[h] = -1;
-	//free(sig.seed);
-	//sig.seed = NULL;
-	sigma.r = sig.r;
-	sigma.s = sig.s;
-	//free(sig.seed);  cannot free, other will be destructed.
-}
-*/
+
 
 void Report(Aux& aux, Srm& srm, Sigma& sigma, const uint8_t* msg, 
             const int T[], const Sig& sig) {
@@ -421,40 +319,18 @@ void Report(Aux& aux, Srm& srm, Sigma& sigma, const uint8_t* msg,
 	sigma.r = sig.r;
 	sigma.s = sig.s;
 }
-/*
-bool judge(SecretKey& sec, PP& pp, const Srm& srm, const uint8_t* mac, uint8_t* hm, 
-    const Sigma& sigma, const mcl::bn::G1& P, const mcl::bn::G2& Q) {
-	
-	if(!Tv(srm, mac)) {
-		return false;
-	}
-	if(!flag) {
-		mcl::bn::Fp12 e1;
-		mcl::bn::pairing(e1, P,Q);
-		mcl::bn::Fp12::pow(e1, e1, sec.x2);
-		e1.serialize(hm, 600);
-		flag = true;
-	}
-	
-	for(int i = 0; i < 32*MSIZE; i++) {
-		hm[i+576] = mac[i];
-	}
-	
-	return Sv(pp, sigma, hm);
-}
 
-*/
 bool Judge(PP& pp, Aux& aux, SecretKey& sec, PublicKey& pks, PublicKey& pkr, 
            const Srm& srm, const Sigma& sigma) {
 	if(!Tv(srm, aux.mac)) {
 		return false;
 	}
-	if(!flag) {
+	if(!flag3) {
 		mcl::bn::Fp12 e1;
 		mcl::bn::pairing(e1, pks.P,pkr.Q);
 		mcl::bn::Fp12::pow(e1, e1, sec.x2);
 		e1.serialize(aux.hm, 600);
-		flag = true;
+		flag3 = true;
 	}
 	
 	for(int i = 0; i < 32*MSIZE; i++) {
